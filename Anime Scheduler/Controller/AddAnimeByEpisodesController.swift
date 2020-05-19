3b9e4ef6f71fa133381b3bcda4e015fcc9452dcd
anshulkala910 <anshulkala910@gmail.com>
2020-05-19 19:14:43 +0900

    removed redundant code by making helper functions

--- Anime Scheduler/Controller/AddAnimeByEpisodesController.swift
+++ Anime Scheduler/Controller/AddAnimeByEpisodesController.swift
@@ -119,15 +119,5 @@ class AddAnimeByEpisodesController: UIViewController {
         let nextDate = theCalendar.date(byAdding: dayComponent, to: startDate)
         return nextDate ?? Date()
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Anime Scheduler/Controller/HomeViewController.swift
+++ Anime Scheduler/Controller/HomeViewController.swift
@@ -64,16 +64,6 @@ class HomeViewController: UIViewController {
         self.currentlyWatchingAnime.append(storedAnime)
         self.currentlyWatchingTableView.reloadData()
     }
-
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
 }
 
 extension HomeViewController: UITableViewDelegate{
