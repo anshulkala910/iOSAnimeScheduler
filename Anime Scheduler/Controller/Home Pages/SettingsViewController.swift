ad7eef9ece9235ca10c811c1306ab1e0b848c099
anshulkala910 <anshulkala910@gmail.com>
2020-10-06 02:03:24 -0400

    Implemented the basic structure of the Settings page

--- Anime Scheduler/Controller/Home Pages/SettingsViewController.swift
+++ Anime Scheduler/Controller/Home Pages/SettingsViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var settingsTableView: UITableView!
+    let columnNames = ["Version", "Developer","Have feedback? Email anshulkala910@gmail.com"]
+    let columnAnswers = ["1.0", "Anshul Kala", ""]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
     }
 
 }
+
+    extension SettingsViewController: UITableViewDelegate{
+    
+    }
+
+extension SettingsViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return columnNames.capacity
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = columnNames[indexPath.row]
+        cell.detailTextLabel?.text = columnAnswers[indexPath.row]
+        return cell
+    }
+    
+    
+    }
