64d7b06ace6d775ed7c615143de5727146a00cf7
anshulkala910 <anshulkala910@gmail.com>
2021-01-08 20:54:45 +0000

    Refactored code and added comments

--- Anime Scheduler/Controller/AddAnimeByEpisodesController.swift
+++ Anime Scheduler/Controller/AddAnimeByEpisodesController.swift
@@ -37,33 +37,40 @@ class AddAnimeByEpisodesController: UIViewController {
         }
     }
     
+    /*
+     This function creates the start date picker that allows user to select a date
+     parameters: none
+     returns: void
+     */
     func createStartDatePicker(){
         startDate.placeholder = getCurrentDate()
         startDate.textAlignment = .center
         let toolbar = UIToolbar()
         toolbar.sizeToFit()
         
+        // add done button
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneButtonPressedStartDate))
         toolbar.setItems([doneButton], animated: true)
+        
         startDate.inputAccessoryView = toolbar
         startDate.inputView = startDatePicker
         startDatePicker.datePickerMode = .date
     }
     
+    /*
+     This function creates the number pad that allows user to input #pes/day
+     */
     func createNumberPad() {
         numberOfEpisodes.placeholder = "1"
         numberOfEpisodes.textAlignment = .center
         let toolbar = UIToolbar()
         toolbar.sizeToFit()
         
+        // add done button
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneButtonNumberPad))
         toolbar.setItems([doneButton], animated: true)
+        
         numberOfEpisodes.inputAccessoryView = toolbar
-    }
-    
-    func getCurrentDate() -> String {
-        let currentDate = Date()
-        return getDateStringFromTextField(currentDate)
     }
     
     @objc func doneButtonPressedStartDate(){
@@ -79,19 +86,36 @@ class AddAnimeByEpisodesController: UIViewController {
         view.endEditing(true)
     }
     
+    /*
+     This function returns the current date as a string
+     parameters: none
+     returns: String
+     */
+    func getCurrentDate() -> String {
+        let currentDate = Date()
+        return getDateStringFromTextField(currentDate)
+    }
+    
+    /*
+     This function returns the string of the date
+     parameters: date
+     returns: String
+     */
+    private func getDateStringFromTextField(_ date: Date) -> String{
+        return dateFormatter.string(from: date)
+    }
+    
+    /*
+     This function shows an alert to the screen when user tries to enter a number more than the number of episodes an anime has
+     parameters: none
+     returns: void
+     */
     func showAlert() {
         let alert = UIAlertController(title: "Error", message: "Invalid Number: Please enter a number that is less than \((animeDetail.episodes ?? 1) + 1) ", preferredStyle: .alert)
         let dismiss = UIAlertAction.init(title: "Dismiss", style: .default , handler: nil)
         alert.addAction(dismiss)
         present(alert, animated: true, completion: nil)
     }
-    
-    //HELPER FUNCTIONS
-    private func getDateStringFromTextField(_ date: Date) -> String{
-        return dateFormatter.string(from: date)
-    }
-    
-    //HELPER FUNCTIONS END
     
     @IBAction func addAnime(_ sender: Any) {
         
@@ -106,26 +130,33 @@ class AddAnimeByEpisodesController: UIViewController {
             alert.addAction(dismiss)
             present(alert,animated: true, completion: nil)
         }
+        
         else{
-            let numberEpisodes = Int(numberOfEpisodes.text ?? "1")
-            if numberEpisodes! > animeDetail.episodes! {
+            let episodesPerDay = Int(numberOfEpisodes.text ?? "1")
+            if episodesPerDay! > animeDetail.episodes! {
                 showAlert()
                 return
             }
             let endDate = getEndDate()
             let endDateString = getDateStringFromTextField(endDate)
             textView.text = "You will finish \(animeDetail.title ?? "...") on \(endDateString)"
+            
             addAnimeButton.isEnabled = true
             addAnimeButton.alpha = 1.0
         }
     }
     
+    /*
+     This function calculates the end date according to the number of episodes/day user wants to watch
+     parameters: none
+     returns: Date representing end date
+     */
     func getEndDate() -> Date {
         let numberEpisodes = animeDetail.episodes
         let numberEpisodesPerDay = Int(numberOfEpisodes.text ?? "1")
         var dayComponent = DateComponents()
         let additionalDays = (numberEpisodes ?? 1)/(numberEpisodesPerDay ?? 1) - 1
-        if (numberEpisodes ?? 1)%(numberEpisodesPerDay ?? 1) != 0{
+        if (numberEpisodes ?? 1)%(numberEpisodesPerDay ?? 1) != 0 {
             dayComponent.day = additionalDays + 1
         }
         else {
